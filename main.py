2d6bce7879b6ce22e24286204dc485d910ef39fb
RashmiKaldera <rashmi98@DESKTOP-C09RJV5.>
2025-05-27 15:25:21 -0400

    Change function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to Greymatter.")
+
 def main():
     print("Hello from greymatter!")
+    username = input("Enter your username: ")
+    greet_user(username)
 
 
 if __name__ == "__main__":
