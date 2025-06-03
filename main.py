7b8f912ecfb5718278024f236313d3c570df3f09
Chris Rauch <crauch333@gmail.com>
2025-06-03 14:36:36 -0400

    chris' function

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ def hello(name):
 def greet(name):
     print(f'This is {name}')
 
+def new_func(name, number):
+    print(f"My name is {name} and my favorite number is {number}")
 
 if __name__ == "__main__":
     hello("world")
     greet('edwin')
     main()
     tasnif()
-
+    new_func("Chris", 5)
